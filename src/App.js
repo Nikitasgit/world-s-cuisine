1acd18e59eba91c354595593fa8fc1c56234d29b
Victor Leman <victorleman1@gmail.com>
2023-05-24 13:25:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "./components/Card";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [inputContent, setInputContent] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://www.themealdb.com/api/json/v1/1/search.php?s=" + inputContent
+      )
+      .then((res) => setData(res.data.meals));
+  }, [inputContent]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>World's Cuisine</h1>
+      <p>A selection of the best recipes from all around the world</p>
+      <input
+        className="input"
+        type="text"
+        placeholder="ex: bolognese"
+        onChange={(e) => setInputContent(e.target.value)}
+      />
+
+      <ul className="meals">
+        {data === null ? (
+          <h3 className="no-results">No results... Try something else!</h3>
+        ) : (
+          data
+            .slice(0, 24)
+            .map((meal) => <Card key={meal.idMeal} meal={meal} />)
+        )}
+      </ul>
     </div>
   );
 }
-
 export default App;
